bb28b907eec697e1e0086c8164a36ad2fd7458a2
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-27 15:58:41 +0900

    feat: view 구현

---
+++ src/main/java/racingcar/View/InputView.java
@@ -0,0 +1,17 @@
+package racingcar.View;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String TRY_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public String readCarNames() {
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String readTryCount() {
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/racingcar/View/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.View;
+
+public class OutputView {
+    void run() {
+        System.out.println("실행 결과");
+        //Winner.java에서 받아온 값 출력
+        System.out.println("최종 우승자: ");
+    }
+}
