3a7eb650c3cba4a904239a810e99e17db70dc756
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-27 17:27:59 +0900

    feat: 유효성 검사 구현

---
+++ src/main/java/racingcar/valid/carNameValid.java
@@ -0,0 +1,33 @@
+package racingcar.valid;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class carNameValid {
+    
+    public carNameValid(String carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("경주할 자동차 이름을 입력해주세요.");
+        }
+
+        if (!carNames.contains(",")) {
+            throw new IllegalArgumentException("자동차 이름은 쉼표(,)로 구분해야 합니다.");
+        }
+        validateCarNames(carNames);
+    }
+
+    public void validateCarNames(String carNames) {
+        List<String> names = Arrays.asList(carNames.split(","));
+
+        names.stream().forEach(name -> {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해야 합니다.");
+            }
+
+            if (!name.matches("[a-zA-Z0-9]+")) {
+                throw new IllegalArgumentException("자동차 이름에 허용되지 않은 문자가 포함되어 있습니다: ");
+            }
+        });
+    }
+
+}
---
+++ src/main/java/racingcar/valid/tryCountValid.java
@@ -0,0 +1,13 @@
+package racingcar.valid;
+
+public class tryCountValid {
+    public tryCountValid(String tryCount) {
+        if (tryCount.isEmpty()) {
+            throw new IllegalArgumentException("시도할 횟수를 입력해주세요.");
+        }
+
+        if (!tryCount.matches("\\d+")) {
+            throw new IllegalArgumentException("시도할 횟수는 숫자여야 합니다.");
+        }
+    }
+}
