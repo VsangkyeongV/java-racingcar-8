0b842c601551b140b9c109b6e30f41d931627ca9
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-27 20:31:33 +0900

    feat: main app 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import racingcar.valid.carNameValid;
+import racingcar.valid.tryCountValid;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
+    private final InputView inputView = new InputView();
+
+    void run() {
+        String carNames = inputView.readCarNames();
+        new carNameValid(carNames);
+        String tryCount = inputView.readTryCount();
+        new tryCountValid(tryCount);
+        Race race = new Race(carNames, tryCount);
+        race.run();
+        String result = race.getResult();
+        String winner = race.getWinner();
+        OutputView output = new OutputView(result, winner);
+        output.run();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Application().run();
     }
 }
