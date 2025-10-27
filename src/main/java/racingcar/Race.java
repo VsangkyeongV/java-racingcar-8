299076ade9bea627dff858e0896eb733ccde7917
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-27 18:52:55 +0900

    feat: Race 구현

---
+++ src/main/java/racingcar/Race.java
@@ -0,0 +1,48 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Race {
+    int tryCount = 0;
+    List<String> carNameList = new ArrayList<>();
+    private List<Car> cars = new ArrayList<>();
+
+    public Race(String carNames, String tryCount) {
+        carNameList = List.of(carNames.split(","));
+        this.tryCount = Integer.parseInt(tryCount);
+    }
+
+    public void makeCars() {
+        this.cars = this.carNameList.stream()
+                    .map(name -> new Car(name))
+                    .collect(Collectors.toList());
+    }
+
+    public boolean isMove() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber >= 4;
+    }
+
+    public void moveCars() {
+        for (Car car : cars) {
+            if (isMove()) {
+                car.move();
+            }
+            car.print();
+        }
+    }
+
+    public void run() {
+        makeCars();
+
+        for (int i = 0; i < tryCount; i++) {
+            moveCars();
+            System.out.println();
+        }
+    }
+    
+}
