862cc6d969dad38c0e41861ad57a93f7de624234
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-27 21:41:48 +0900

    feat: 중복불가 유효성 추가

--- src/main/java/racingcar/valid/carNameValid.java
+++ src/main/java/racingcar/valid/carNameValid.java
@@ -27,6 +27,9 @@ public final class CarNameValid {
             if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력해야 합니다.");
             }
+            if (names.size() != names.stream().distinct().count()) {
+                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+            }
         });
     }
 
