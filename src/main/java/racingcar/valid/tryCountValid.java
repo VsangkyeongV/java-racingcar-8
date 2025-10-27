a806ac26ecf712659aaf50a175283bd3101adbc0
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-10-27 22:42:33 +0900

    fix: 오류찾기

--- src/main/java/racingcar/valid/tryCountValid.java
+++ src/main/java/racingcar/valid/tryCountValid.java
@@ -7,8 +7,8 @@ public final class TryCountValid {
             throw new IllegalArgumentException("시도할 횟수를 입력해주세요.");
         }
 
-        if (!tryCount.matches("\\d+")) {
-            throw new IllegalArgumentException("시도할 횟수는 숫자여야 합니다.");
-        }
+        // if (!tryCount.matches("\\d+")) {
+        //     throw new IllegalArgumentException("시도할 횟수는 숫자여야 합니다.");
+        // }
     }
 }
